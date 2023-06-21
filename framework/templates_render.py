d28047163e3d834408b67f878c9aba94b571d2f0
AlexandrReznik <saha.reznik2004@gmail.com>
2023-06-22 00:29:57 +0300

    Changed templator

--- framework/templates_render.py
+++ framework/templates_render.py
@@ -1,19 +1,9 @@
 from jinja2 import Template
 import os
 
-# def render(template_name, folder='coulour_purple', **kwargs):
-#     file_path = os.path.join(folder, template_name)
-#     with open(file_path, encoding='utf-8') as f:
-#         template = Template(f.read())
-#     return template.render(**kwargs)
-
-
 
 def render(template_name, folder='templates', **kwargs):
     file_path = os.path.join(folder, template_name)
-    # Открываем шаблон по имени
     with open(file_path, encoding='utf-8') as f:
-        # Читаем
         template = Template(f.read())
-    # рендерим шаблон с параметрами
     return template.render(**kwargs)
